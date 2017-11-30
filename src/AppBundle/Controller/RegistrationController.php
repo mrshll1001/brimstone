9cc15d6bb396a2ef6783f85c0a81c7110d22990a
Matt Marshall <matt@mrshll.uk>
2017-11-30 23:03:30 +0000

    Setup performs check for existing user, if found it prevents registration from happening again

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -12,37 +12,70 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends Controller
 {
-    /**
-     * @Route("/register", name="user_registration")
-     */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+  /**
+  * Handles set up of the initial user account
+  */
+  public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+  {
+    // ==================================================
+    /* In Brimstone there should only ever be one user.
+    So perform a check to see if there's a user already. */
+    // ======================================================
+
+    $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();  // Get The user collection.
+
+    // Check the collection is empty
+    if (empty($users))
     {
-        // 1) build the form
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+      // 0) Collection is empty, there are no users -- we can proceed to display the setup form
+      // 1) build the form
+      $user = new User();
+      $form = $this->createForm(UserType::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+      // 2) handle the submit (will only happen on POST)
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
+        // 3) Encode the password (you could also do this via Doctrine listener)
+        $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+        $user->setPassword($password);
 
-            // 4) save the User!
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+        // 4) save the User!
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
+        // ... do any other work - like sending them an email, etc
+        // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('index');
-        }
+        return $this->redirectToRoute('index');
+      }
 
-        return $this->render(
-            'AppBundle:registration:register.html.twig',
-            array('form' => $form->createView())
-        );
+      return $this->render(
+        'AppBundle:registration:register.html.twig',
+        array('form' => $form->createView())
+      );
+    } else
+    {
+      // =======================================================================
+      // We have a user, so we should redirect the request appropriately
+      // =======================================================================
+
+      // Check their login status
+      if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY'))
+      {
+        // They're logged in, redirect them to the control panel
+        return $this->redirectToRoute('control_panel');
+      } else
+      {
+        // They're not logged in, redirect them back to the 'home' page.
+        return $this->redirectToRoute('index');
+      }
+
     }
+
+
+
+
+  }
 }
