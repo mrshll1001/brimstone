425923325b216a0aff1fc18a87df3b2d3fc1fa6c
Matt Marshall <matt@mrshll.uk>
2017-12-03 13:30:46 +0000

    Handles lack of profile object

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -6,6 +6,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/* Entities */
+use AppBundle\Entity\User;
+
+/* Exceptions */
+use AppBundle\Exception\NullProfileException;
+
 /**
  * Provides controllers for Protected actions such as the control panel and creating content
  */
@@ -17,7 +23,33 @@ class AdminController extends Controller
    */
   public function controlPanelAction(Request $request)
   {
-    return $this->render('AppBundle:admin:control_panel.html.twig', array());
+
+    /* try to execute the code, but there's no profile yet we need to set it up */
+    try
+    {
+      $user = $this->getUser(); // Get the User
+      $this->checkUser($user);  // Check stuff about them
+
+
+      return $this->render('AppBundle:admin:control_panel.html.twig', array());
+
+    } catch (NullProfileException $e)
+    {
+
+      return $this->redirectToRoute('configure_initial_profile'); // Redirect to the configuration page
+    }
+
+
+
+
+  }
+
+  /**
+   * After logging in for the first time users should be directed here to initialise their UserProfile object
+   */
+  public function configureInitialProfileAction(Request $request)
+  {
+    return $this->render('AppBundle:admin:configure_initial_profile.html.twig', array());
   }
 
 
@@ -28,5 +60,19 @@ class AdminController extends Controller
   protected function getUser()
   {
     return $this->get('security.token_storage')->getToken()->getUser();
+  }
+
+  /**
+   * Performs various checks on the user and redirects appropriately based on the resulting conditions
+   */
+  protected function checkUser(User $user)
+  {
+    echo $user->getProfile();
+    /* If user's UserProfile object is null, redirect them to the Configuration */
+    if (is_null($user->getProfile()))
+    {
+      throw new NullProfileException();
+    }
+
   }
 }
---
+++ src/AppBundle/Exception/NullProfileException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Exception;
+/**
+ * Exception Thrown when user's profile is null
+ */
+class NullProfileException extends \Exception
+{
+
+}
