60910c3abe7d0a183a49deacea545f72866462b7
Matt Marshall <matt@mrshll.uk>
2017-12-06 21:16:26 +0000

    Public facing routes and templates for about page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,8 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * Default page
+    /**===========================================================================================
+     * Home page
+     * ===========================================================================================
      */
     public function indexAction(Request $request)
     {
@@ -27,5 +28,30 @@ class DefaultController extends Controller
 
 
         return $this->render('AppBundle:public:index.html.twig', array('profile' => $userProfile));
+    }
+
+    /**===========================================================================================
+     * About page -- should redirect to home if user->profile->about === null
+     * ===========================================================================================
+     */
+    public function aboutAction(Request $request)
+    {
+      /* First, try to load the user object */
+      $user = $this->getDoctrine()->getRepository('AppBundle:User')->getSingleUser();
+
+      /* If the user object is null, then Brimstone hasn't been set up, so load the template that says so */
+      if ($user === null)
+      {
+        return $this->render('AppBundle:public:not_setup.html.twig', array());
+      }
+
+      /* Check if the user profile is null, if it is then they probably don't want to show a blank page so redirect */
+      if ($user->getProfile()->getAbout() === null)
+      {
+        return $this->redirectToRoute('index');
+      }
+
+      /* We can load the page, so we can just get the profile object which contains about anyway */
+      return $this->render('AppBundle:public:about.html.twig', array('profile' => $user->getProfile() ));
     }
 }
