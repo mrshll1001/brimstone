c10630263479eec5ef0d17ec66656c2deffdc034
Matt Marshall <matt@mrshll.uk>
2017-12-06 08:21:49 +0000

    Began on public facing side. Logic to check if current user is null, and began public templates

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,10 +13,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+      /* First, try to load the user object */
+      $user = $this->getDoctrine()->getRepository('AppBundle:User')->getSingleUser();
 
-        // replace this example code with whatever you need
-        return $this->render('AppBundle:public:index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+      /* If the user object is null, then Brimstone hasn't been set up, so load the template that says so */
+      if ($user === null)
+      {
+        return $this->render('AppBundle:public:not_setup.html.twig', array());
+      }
+
+      /* Sweet, we can load the page */
+      $userProfile = $user->getProfile();     // Posts are loaded separately, so we only need to pass in the user profile for the navbar.
+
+
+        return $this->render('AppBundle:public:index.html.twig', array('profile' => $userProfile));
     }
 }
